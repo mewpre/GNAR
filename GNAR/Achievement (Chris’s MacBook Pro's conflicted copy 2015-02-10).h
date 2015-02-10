a31ab3bc4394af24ea5027519b3a4c6ee120ab2f
Chris Giersch <chgiersch@Chriss-MacBook-Pro.local>
2015-02-10 13:30:47 -0800

    i hate github

---
+++ GNAR/Achievement (Chris’s MacBook Pro's conflicted copy 2015-02-10).h
@@ -0,0 +1,36 @@
+//
+//  Achievement.h
+//  GNAR
+//
+//  Created by Chris Giersch on 2/10/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface Achievement : PFObject <PFSubclassing>
+
+@property (retain) NSString *name;
+@property (retain) NSString *abbreviation;
+@property (retain) NSArray *pointValues;
+//@property (retain) CLLocation *location;
+@property BOOL isAvailable;
+@property BOOL isCustom;
+@property (retain) NSString *type;
+@property (retain) NSString *snowLevel;
+//@property double timeLimiter;
+@property (retain) NSString *description;
+@property (retain) NSString *difficulty;
+@property (retain) NSString *heroFactor;
+@property (retain) NSString *funFactor;
+<<<<<<< HEAD
+
+
+=======
+>>>>>>> work
+@property (retain) NSDate *completedAt;
+
+
++ (NSString *)parseClassName;
+
+@end
