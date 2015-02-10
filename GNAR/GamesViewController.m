4229ffa339beb2c12c4e4eefe892e9cdeb791cba
Chris Giersch <chgiersch@Chriss-MacBook-Pro.local>
2015-02-09 17:15:14 -0800

    added games view controller and addGames view controller and storyboard respective views

---
+++ GNAR/GamesViewController.m
@@ -0,0 +1,52 @@
+//
+//  GamesViewController.m
+//  GNAR
+//
+//  Created by Chris Giersch on 2/9/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import "GamesViewController.h"
+
+@interface GamesViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+@end
+
+@implementation GamesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+//----------------------------------------    Table View    ----------------------------------------------------
+#pragma mark - Table View
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    return cell;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
