7908e916ab5b820afcae4df348a955e0ad00dad5
Chris Giersch <chgiersch@Chriss-MacBook-Pro.local>
2015-02-09 21:47:52 -0800

    finished storyboard

--- GNAR/UserAchievementsViewController.m
+++ GNAR/UserAchievementsViewController.m
@@ -1,45 +1,24 @@
 //
-//  AchievementViewController.m
+//  UserAchievementsViewController.m
 //  GNAR
 //
 //  Created by Chris Giersch on 2/9/15.
 //  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
 //
 
-#import "AchievementViewController.h"
+#import "UserAchievementsViewController.h"
 
-@interface AchievementViewController ()
-
+@interface UserAchievementsViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
-@implementation AchievementViewController
+@implementation UserAchievementsViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
-
-//----------------------------------------    Table View    ----------------------------------------------------
-#pragma mark - Table View
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 0;
-}
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    return cell;
-}
-
-
-
-
-
-
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
