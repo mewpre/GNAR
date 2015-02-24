a6ee9768310594d740180c4ba1609bdf84795124
Chris Giersch <chgiersch@Chriss-MacBook-Pro.local>
2015-02-23 22:36:39 -0800

    adding custom cell to LeaderboardVC

---
+++ GNAR/LeaderboardTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  LeaderboardTableViewCell.h
+//  GNAR
+//
+//  Created by Chris Giersch on 2/23/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeaderboardTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rankLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreRatioLabel;
+
+@end
