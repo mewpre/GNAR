cf134fa20876b819d412d9e0b42f3e01a8a5c114
Chris Giersch <chgiersch@Chriss-MacBook-Pro.local>
2015-02-25 21:26:57 -0800

    storyboard updates

--- SuggestedTableViewCell.m
+++ SuggestedTableViewCell.m
@@ -19,5 +19,9 @@
 
     // Configure the view for the selected state
 }
+- (IBAction)onAcceptButtonPressed:(UIButton *)sender {
+}
+- (IBAction)onDeclineButtonPressed:(UIButton *)sender {
+}
 
 @end
